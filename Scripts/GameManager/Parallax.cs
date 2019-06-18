4e0e2e2747b06386ec59317cce37bfd493c88997
jackchannn <48765654+jackchannn@users.noreply.github.com>
2019-06-18 14:52:07 +0800

    Create Parallax.cs
    添加了视差滚动的脚本

---
+++ Scripts/GameManager/Parallax.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform[] backgrounds;
+    public float parallaxScale;
+    public float parallaxReduxtionFactor;
+    public float smoothing;
+
+
+    private Transform cam;
+    private Vector3 previousCamPos;
+
+    void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        previousCamPos = cam.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UseParallax();
+        previousCamPos = cam.position;
+    }
+    void UseParallax()
+    {
+        float parallaxX = (previousCamPos.x - cam.position.x) * parallaxScale;
+        float parallaxY = (previousCamPos.y - cam.position.y) * parallaxScale;
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            float backgroundTargetPosX = backgrounds[i].position.x + parallaxX * (i * parallaxReduxtionFactor + 1);
+            float backgroundTargetPosY = backgrounds[i].position.y + parallaxY * (i * parallaxReduxtionFactor + 1);
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgroundTargetPosY, backgrounds[i].position.z);
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing);
+        }
+    }
+}
