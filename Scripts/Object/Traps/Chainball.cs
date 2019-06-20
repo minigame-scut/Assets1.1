091ec5c4c169ec3954ec6c22010a6fd2251a6cd6
pinkDump <32223238+upandown@users.noreply.github.com>
2019-06-20 17:52:35 +0800

    --

---
+++ Scripts/Object/Traps/Chainball.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class Chainball : MonoBehaviour
+{
+    public Vector3 point;
+
+    //旋转速度
+    public  float speed = 5f;
+
+    float speedRate = 10;
+    float angle = 0;
+    //围绕点距离中心多远
+     float dis = 1.5f;
+    float limitAngle = 62f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //获得围绕的点的位置
+        point = GetComponentInChildren<Transform>().position;
+        angle = 10 * Time.deltaTime * speed;
+
+        //float newAngle = (transform.localEulerAngles.z >= 180) ? (transform.localEulerAngles.z - 360) : (transform.localEulerAngles.z);
+        //Debug.Log(newAngle);
+        //point.x = point.x - dis * (float)Math.Sin(newAngle / (2*Math.PI));
+        //point.y = point.y + dis * (float)Math.Cos(newAngle / (2*Math.PI));
+        //Debug.Log(point);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (GameObject.Find("player 1(Clone)") == null)
+        //{
+        //    speed = 5;
+        //}
+
+        float increase = 0f;
+        //反向
+        float newAngle = (transform.localEulerAngles.z >= 180) ? (transform.localEulerAngles.z - 360) : (transform.localEulerAngles.z);
+
+        //计算速度
+         speed = speed< 0? -((1-Math.Abs(newAngle) / limitAngle) * speedRate + 2): ((1-Math.Abs(newAngle) / limitAngle) * speedRate + 2);
+       
+        if (newAngle > limitAngle || newAngle < -limitAngle)
+        {
+            speed *= -1;
+            if (speed > 0)
+                increase = 1;
+            else
+                increase = -1;
+        }
+
+        //  transform.Rotate(Vector3.forward, 10 * Time.deltaTime * speed);
+        angle = 10 * Time.deltaTime * speed;
+        //transform.RotateAround(point, Vector3.forward, angle);
+        transform.Rotate(Vector3.forward, angle + increase);
+    }
+
+    //玩家触碰死亡
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if(coll.tag == "player" && !GameManager.instance.getSceneManager().GetComponent<SManager>().getGamePlayer().GetComponent<PlayerPlatformController>().getPlayerData().isDead)
+        {
+            EventCenter.Broadcast(MyEventType.DEATH);
+            speed *= -1;
+            Debug.Log(speed);
+        }
+    }
+}
---
+++ Scripts/ScenesManager/SwitchMap.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchMap : MonoBehaviour
+{
+    private Animator ani;//获得动画控制器组件
+
+    void Start()
+    {
+        ani = this.GetComponent<Animator>();
+        //  ani.SetBool("closemap", true);
+        ani.SetBool("openmap", true);
+        Debug.Log("switcher");
+    }
+
+    //播放开始动画
+    public void PlayOpenMap()
+    {
+        ani.SetBool("openmap", true);
+    }
+    //停止播放开始动画
+    public void StopOpenMap()
+    {
+        ani.SetBool("openmap", false);
+    }
+
+    //播放关闭动画
+    public void PlayCloseMap()
+    {
+        ani.SetBool("closemap", true);
+    }
+    //停止播放关闭动画 
+    public void StopCloseMap()
+    {
+        ani.SetBool("closemap", false);
+    }
+}
