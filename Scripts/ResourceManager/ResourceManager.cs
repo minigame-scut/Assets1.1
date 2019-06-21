10e2998fa8dfd07989208c78e222c67b0af5e2cd
BruceAtian <36923246+BruceAtian@users.noreply.github.com>
2019-06-21 08:50:16 +0800

    Update ResourceManager.cs

--- Scripts/ResourceManager/ResourceManager.cs
+++ Scripts/ResourceManager/ResourceManager.cs
@@ -13,6 +13,7 @@ public class ResourceManager : MonoBehaviour
 
     public Dictionary<string, AudioClip> musicDictionary;
     public Dictionary<string, GameObject> gameObjectDictionary;
+    public Dictionary<string, Material> materialDictionary;
     public static ResourceManager GetInstance()
     {
             return instance;
@@ -35,6 +36,7 @@ public class ResourceManager : MonoBehaviour
         }
         musicDictionary = new Dictionary<string, AudioClip>();
         gameObjectDictionary = new Dictionary<string, GameObject>();
+        materialDictionary = new Dictionary<string, Material>();
         init();
     }
 
@@ -64,6 +66,7 @@ public class ResourceManager : MonoBehaviour
         gameObjectDictionary.Add("GameManagerRes/player", Resources.Load<GameObject>("GameManagerRes/player"));
         gameObjectDictionary.Add("GameManagerRes/AudioManager", Resources.Load<GameObject>("GameManagerRes/AudioManager"));
         gameObjectDictionary.Add("GameManagerRes/UI", Resources.Load<GameObject>("GameManagerRes/UI"));
+        materialDictionary.Add("Materials/lightM", Resources.Load<Material>("Materials/lightM"));
     }
     public AudioClip getClip(string name)
     {
@@ -75,6 +78,12 @@ public class ResourceManager : MonoBehaviour
     {
         if (gameObjectDictionary.ContainsKey(name))
             return gameObjectDictionary[name];
+        return null;
+    }
+    public Material getMaterial(string name)
+    {
+        if (materialDictionary.ContainsKey(name))
+            return materialDictionary[name];
         return null;
     }
 }
