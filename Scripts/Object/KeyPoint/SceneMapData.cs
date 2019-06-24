98bc72556d6b65b14b1eb59a9d4b1b95ab71bf3b
pinkDump <32223238+upandown@users.noreply.github.com>
2019-06-24 10:19:04 +0800

    -

--- Scripts/Object/KeyPoint/SceneMapData.cs
+++ Scripts/Object/KeyPoint/SceneMapData.cs
@@ -47,7 +47,13 @@ public class SceneMapData
             mapData.Add("nextPlace1-3-2", "birthPlace1-6-1");
             mapData.Add("nextPlace1-6-1", "birthPlace1-3-2");
             mapData.Add("nextPlace1-4-2", "birthPlace1-5-1");
-            mapData.Add("nextPlace1-6-2", "birthPlace1-8-1");
+            mapData.Add("nextPlace1-6-2", "birthPlace2-0-1");
+            mapData.Add("nextPlace1-6-2", "birthPlace2-0-1");
+            mapData.Add("nextPlace2-0-1", "birthPlace2-1-1");
+            mapData.Add("nextPlace2-1-1", "birthPlace2-2-1");
+            mapData.Add("nextPlace2-2-1", "birthPlace2-3-1");
+            mapData.Add("nextPlace2-3-1", "birthPlace2-4-1");
+            mapData.Add("nextPlace2-4-1", "birthPlace2-5-1");
 
         //传送门映射
         mapData.Add("transDoor1-1-1-1", "transDoor1-1-1-2");
@@ -56,7 +62,8 @@ public class SceneMapData
         mapData.Add("transDoor1-5-2-1", "transDoor1-1-2-2");
         mapData.Add("transDoor1-3-3-2", "transDoor1-7-3-1");
         mapData.Add("transDoor1-7-3-1", "transDoor1-3-3-2");
-
+        mapData.Add("transDoor2-5-1-1", "transDoor2-5-1-2");
+        mapData.Add("transDoor2-5-1-2", "transDoor2-5-1-1");
         //里表世界门映射
         mapData.Add("inworldDoor1-3-1", "outworldDoor1-7-1");
         mapData.Add("outworldDoor1-7-1", "inworldDoor1-3-1");
@@ -69,7 +76,6 @@ public class SceneMapData
         mapData.Add("inworldDoor2-3-2", "outworldDoor2-6-2");
         mapData.Add("outworldDoor2-6-2", "inworldDoor2-3-2");
 
-
         //地图编号与build编号的映射
         mapData.Add("map1-0", "1");
         mapData.Add("map1-1", "2");
@@ -79,7 +85,7 @@ public class SceneMapData
         mapData.Add("map1-5", "6");
         mapData.Add("map1-6", "7");
         mapData.Add("map1-7", "8");
-        mapData.Add("map1-8", "9");
+        mapData.Add("map2-0", "9");
         mapData.Add("map2-1", "10");
         mapData.Add("map2-2", "11");
         mapData.Add("map2-3", "12");
