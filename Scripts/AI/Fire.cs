2520de91354a8766e50d88196971fcb950efbdb1
Chenxvil <1229029461@qq.com>
2019-06-24 16:33:01 +0800

    map2-5

---
+++ Scripts/AI/Fire.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("子弹")]
+    public GameObject fire;
+
+    [Header("最长发射间隔")]
+    public int fireCD = 300;
+    private int cd;
+    private int timer;
+    void Start()
+    {
+        cd = fireCD;
+        timer =0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("Fire(Clone)") != null||GameObject.FindGameObjectWithTag("player")==null)
+            timer=0;
+        timer++;
+        if (timer >= cd )
+        {
+            cd = Random.Range(60, fireCD);
+            Vector3 temp = new Vector3(transform.position.x + (cd % 40), transform.position.y, transform.transform.position.z);
+            Instantiate<GameObject>(fire, temp,transform.rotation);
+            
+            timer = 0;
+        }
+    }
+
+}
---
+++ Scripts/AI/track.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class track : MonoBehaviour
+{
+    [Header("生存时间")]
+    public int live = 60;
+    private int timer;
+
+    [Header("追踪速度")]
+    public float trackspeed = 10;
+
+    [Header("追踪目标名字")]
+    public string objname;
+    private GameObject trackobj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+        if (GameObject.Find(objname) != null)
+            trackobj = GameObject.Find(objname);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(trackobj==null)
+        {
+            trackobj = GameObject.Find(objname);
+            if (trackobj == null)
+            {
+                Destroy(gameObject);
+                return;
+            }
+        }
+        if(timer >= live )
+        {
+            Destroy(gameObject);
+        }
+
+        Track(trackobj.transform.position);
+        timer++;
+    }
+    void Track(Vector3 dst)
+    {
+        Vector3 forward = dst - transform.position;
+        forward = Vector3.Normalize(forward);
+        transform.Translate(forward * trackspeed * Time.deltaTime);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "player")
+        {
+            Destroy(gameObject);
+            EventCenter.Broadcast(MyEventType.DEATH);
+        }
+    }
+}
